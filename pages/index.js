392ecaee6017e7aec3a4e55b7e911990865595c8
Anup Shrestha <xxanup.sthaxx@gmail.com>
2021-05-22 15:08:15 +0545

    About Screen Added

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import LandingScreen from "../components/layout/LandingScreen";
-
+import AboutScreen from "../components/layout/AboutScreen";
 const HomePage = () => {
   return (
     <>
-      <LandingScreen></LandingScreen>
+      <LandingScreen />
+      <AboutScreen />
     </>
   );
 };
