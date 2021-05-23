36829ba0cd5a459978419e977283497fc60f0722
Anup Shrestha <xxanup.sthaxx@gmail.com>
2021-05-23 13:50:28 +0545

    Review Section Added

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import LandingScreen from "../components/layout/LandingScreen";
 import AboutScreen from "../components/layout/AboutScreen";
 import FeatureScreen from "../components/layout/FeatureScreen";
 import ProductSection from "../components/layout/ProductSection";
+import ReviewSection from "../components/layout/ReviewSection";
 const HomePage = () => {
   return (
     <>
@@ -9,6 +10,7 @@ const HomePage = () => {
       <AboutScreen />
       <FeatureScreen />
       <ProductSection />
+      <ReviewSection />
     </>
   );
 };
